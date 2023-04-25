b66562e550c0e2642682a99ac541eb132227a793
hanjaegyeong <hjg000223@naver.com>
2023-04-25 15:41:18 +0900

    feat: 로그 기능 추가

--- doorlock/app.py
+++ doorlock/app.py
@@ -1,12 +1,15 @@
 import random
+from datetime import datetime
 from flask import Flask,render_template, render_template, request
 app = Flask(__name__)
+
+log_list = []
 
 #메인 페이지
 @app.route('/',methods=['GET','POST'])
 def main():
     if request.method == 'POST':
-        random_list = list(range(0,10)) #버튼 클릭 -> 첫 번째 패스워드 선택 창
+        random_list = list(range(0,10)) #버튼 클릭 -> 첫 번째 패스워드 선택화면
         random_list.append('*')
         random_list.append('#')
         randoms = random.sample(random_list, 12)
@@ -16,12 +19,20 @@ def main():
 #패스워드 맞을 때
 @app.route('/one',methods=['GET','POST'])
 def one():
-    password = '1' #첫번째 패스워드 설정
-    number = request.args["number"]
+    password = '1' #password: 첫번째 패스워드 설정
+    number = request.args["number"] #number: 사용자가 입력한 패스워드
+
+    #입력받은 값, 시간은 로그에 추가
+    now = datetime.now()
+    input_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    log_list.append([number, input_time])
+
+    #랜덤 순서 만들기
     random_list = list(range(0,10))
     random_list.append('*')
     random_list.append('#')
     randoms = random.sample(random_list, 12)
+
     if password == number:
         return render_template('doorlock_two.html', numbers = randoms)
     else:
@@ -31,10 +42,16 @@ def one():
 def two():
     password = '2' #두번째 패스워드 설정
     number = request.args["number"]
+
+    now = datetime.now()
+    input_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    log_list.append([number, input_time])
+
     random_list = list(range(0,10))
     random_list.append('*')
     random_list.append('#')
     randoms = random.sample(random_list, 12)
+
     if password == number:
         return render_template('doorlock_three.html', numbers = randoms)
     else:
@@ -44,10 +61,16 @@ def two():
 def three():
     password = '3' #세번쨰 패스워드 설정
     number = request.args["number"]
+
+    now = datetime.now()
+    input_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    log_list.append([number, input_time])
+
     random_list = list(range(0,10))
     random_list.append('*')
     random_list.append('#')
     randoms = random.sample(random_list, 12)
+
     if password == number:
         return render_template('doorlock_four.html', numbers = randoms)
     else:
@@ -57,10 +80,11 @@ def three():
 def four():
     password = '4' #세번째 패스워드 설정
     number = request.args["number"]
-    random_list = list(range(0,10))
-    random_list.append('*')
-    random_list.append('#')
-    randoms = random.sample(random_list, 12)
+
+    now = datetime.now()
+    input_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    log_list.append([number, input_time])
+
     if password == number:
         return render_template('correct.html')
     else:
@@ -70,6 +94,11 @@ def four():
 #패스워드 틀렸을 때 -> wrong 경로로
 @app.route('/wrong_two',methods=['GET','POST'])
 def wrongTwo():
+    number = request.args["number"]
+    now = datetime.now()
+    input_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    log_list.append([number, input_time])
+    
     random_list = list(range(0,10))
     random_list.append('*')
     random_list.append('#')
@@ -78,6 +107,11 @@ def wrongTwo():
 
 @app.route('/wrong_three',methods=['GET','POST'])
 def wrongThree():
+    number = request.args["number"]
+    now = datetime.now()
+    input_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    log_list.append([number, input_time])
+    
     random_list = list(range(0,10))
     random_list.append('*')
     random_list.append('#')
@@ -86,17 +120,26 @@ def wrongThree():
 
 @app.route('/wrong_four',methods=['GET','POST'])
 def wrongFour():
+    number = request.args["number"]
+    now = datetime.now()
+    input_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    log_list.append([number, input_time])
+    
     random_list = list(range(0,10))
     random_list.append('*')
     random_list.append('#')
     randoms = random.sample(random_list, 12)
     return render_template('wrong.html', numbers = randoms)
 
-
 #wrong.html에서 다시 시작하기 버튼 눌렀을 때
 @app.route('/again', methods=['POST'])
 def tmp():
     return render_template('main.html')
+
+#사용자가 입력한 패스워드 전부 확인 가능한 log페이지
+@app.route('/log',methods=['GET','POST'])
+def log():
+    return render_template('log.html', log_list = log_list)
 
 if __name__ == '__main__':
    app.run(port = 5000, debug = True)
